5c64221af9775b818c8efc800c7b8401ca687fc9
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-04 23:01:10 +0100

    improved templating

--- src/templates/email.ts
+++ src/templates/email.ts
@@ -1,7 +1,17 @@
-export const basic = () => {
+export const basic = (user: String, email: String, message: String) => {
   return `
+  <div class="container" style="font-size:16px;">
+  <p>Hello there! </p>
+  <p>You have received a new message from <b> ${user} </b> (${email})</p>
   <div>
-    <h1>Basic Template</h1>
+  <p style="background:#e8e8e8; border: 0.5px black; margin: 0; padding: 20px; padding-left: 2.25%; padding-right: 6.25%; width: 50%; font-size: 17px; font-weight: 400; line-height: 160%;">
+    ${message}
+  </p>
+  </div>
+  <p style="cursor:pointer; font-size: 17px;"><a style="text-decoration:none; color: #127DB3; font-weight: bold;" href="mailto:${email}">Click here to reply</a></p><br>
+  <h4> See you soon! </h4>
+  <hr>
+  This e-mail was sent from <b>TS Mailer</b> 😃
   </div>
   `;
 };
