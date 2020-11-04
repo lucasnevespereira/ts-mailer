9f00fb17d237817e4015e00c19469cf85c6a5fe7
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-04 23:02:08 +0100

    added mailer logic

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,9 +1,43 @@
 import dotenv from "dotenv"
+import nodemailer from "nodemailer"
+import * as template from "../templates/email"
 
 // Load env variables
 dotenv.config()
 
-export const sendMail = (data: JSON) => {
-  console.log(`Got your data: ${data}`)
-  return data
+interface emailData {
+  name: String,
+  email: String,
+  message: String,
+  to: String
+}
+
+export const sendMail = async (data: emailData) => {
+
+  // Destructuring Data Info
+  const { name, email, message, to } = data
+  const defaultTo = "ts.mailer@gmail.com"
+
+  // Creating a SMTP Transporter
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true if port is 465
+    auth: {
+      user: process.env.AUTH_EMAIL,
+      pass: process.env.AUTH_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `ts.mailer@gmail.com`, // sender address
+    to: `${to || defaultTo}`, // list of receivers
+    subject: `TS Mailer: New Message`, // Subject line
+    html: template.basic(name, email, message),
+  }
+
+  // Send mail tru transport and wait result
+  const info = await transporter.sendMail(mailOptions)
+
+  return info
 }
