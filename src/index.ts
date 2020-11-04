8327368eb168c10ea1dcbc3079f7d59cb4b0f8a5
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-04 21:40:02 +0100

    init basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello from TS-Mailer")
+import express, { Request, Response } from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+
+
+const app = express()
+
+// MiddleWares
+app.use(cors({ origin: '*' }))
+app.use(bodyParser.json())
+
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to TS-Mailer!')
+})
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('TS-Mailer is running');
+})
